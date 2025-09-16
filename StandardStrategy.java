530b3f916d9a85cce4e9bf2d54a7eae7f3a64817
Choi Dong-bin <43774570+DBChoi85@users.noreply.github.com>
2025-09-16 22:27:09 +0900

    Create StandardStrategy.java

---
+++ StandardStrategy.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public final class StandardStrategy implements PricingStrategy {
+    @Override
+    public BigDecimal quote(Booking b) {
+        return b.roomType().baseRate()
+                .multiply(BigDecimal.valueOf(b.nights()))
+                .setScale(0, RoundingMode.HALF_UP);
+    }
+}
